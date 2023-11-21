5b57458cc8fda01918472fa38b6e80cc9fd044fb
nrthbnd <asoloshenkova@gmail.com>
2023-11-21 11:33:27 +0300

    Updates

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,6 @@
-from pydantic import BaseSettings
+from typing import Optional
+
+from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
@@ -6,6 +8,20 @@ class Settings(BaseSettings):
     description: str
     database_url: str
     secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+    # Переменные для Google API
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
 
     class Config:
         env_file = '.env'
--- app/models/reservation.py
+++ app/models/reservation.py
@@ -4,6 +4,7 @@ from app.core.db import Base
 
 
 class Reservation(Base):
+    """Модель благотворительного проекта."""
     from_reserve = Column(DateTime)
     to_reserve = Column(DateTime)
     meetingroom_id = Column(Integer, ForeignKey('meetingroom.id'))
